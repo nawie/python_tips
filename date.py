6fc8fe9f9440fc4bd1d17ad6d6dc43a209d47282
nawie <elop.development@gmail.com>
2016-05-12 15:46:43 +0700

    get date like php
    get date like php format, with delta time in day

---
+++ date.py
@@ -0,0 +1,19 @@
+# get date like php format
+#
+# example   : date('d-M-Y'), minus 1 day date('d-M-Y', -1), plus 1 day date('d-M-Y', 1)
+# result    : 12-05-2016, 11-05-2016, 13-05-2016,
+#
+def date(format = "", delta = 0):
+    assert isinstance(format, basestring), "Date format must be a string"
+    assert isinstance(delta, int), "Date delta must be an integer"
+    if not format :
+        format = 'Y-m-d H:M:S'
+    # check date part
+    def part(x):
+        return '%'+x if re.match("[^\W\d_]",x) else x
+    # check is delta
+    now = datetime.datetime.now() + datetime.timedelta(days=delta)
+    # result
+    return now.strftime(
+            ''.join(map(part, format))
+        )
